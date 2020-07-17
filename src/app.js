cc74c2079f5d9227ab3f08d796c0c44944069537
unknown <akramsheriff05042000@gmail.com>
2020-07-17 06:32:26 -0400

    added about page links

--- src/app.js
+++ src/app.js
@@ -2,16 +2,30 @@
 const path=require('path')
 
 const express=require('express')
+
 const app=express()
 // path set pana html file ku
 const pathway=path.join(__dirname,'../public')
+
+// hbs concept set pana
+app.set('view engine','hbs')
 
 //  express la html use pana
  app.use(express.static(pathway))
 
  app.get('',(req,res)=>{
-    res.send('hi')
+    res.render('index',{
+        title:'CalCulator',
+        credits:'Akram sheriff'
+    })
 })
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About',
+        credits:'Akram sheriff'
+    })
+})
+
 
 
 app.listen(8080,()=>{
